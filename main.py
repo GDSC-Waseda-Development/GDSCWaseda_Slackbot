6c8fe0980cc031916c16677540c4befa8d5e208a
YCHX <ychx.alvin@gmail.com>
2023-06-01 21:13:41 +0900

    6/1 app mention

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 import os
 from dotenv import load_dotenv
+import re
+
+channel_ids = {"backend":"C02HKKFAH8B","frontend":"whoknows"}
 
 load_dotenv()
 
@@ -19,6 +22,19 @@ def message_hello(message, say):
 def message_see_you(message, say):
     say(f"See you! <@{message['user']}>!")
 
+@app.event("message")
+def handle_message_events(body, logger):
+    pass
+
+@app.event("app_mention")
+def mention_handler(event, say):
+    channel_id = event["channel"]
+    if channel_id == channel_ids["backend"]:
+        text = re.sub('\\s<@[^, ]*|^<@[^, ]*', '', event['text'])
+        if 'check' in text:
+            user_info = app.client.users_info(user=event["user"])
+            user_name = user_info["user"]["profile"]["display_name"]
+            say(f"Hi, Checked {user_name}")
 
 # Handler must be defined above this or it cannot be used
 if __name__ == "__main__":
